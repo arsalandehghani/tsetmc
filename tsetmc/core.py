f7a25bfdec4eaadd15d4c1cf5f7de26bcfdd2e76
Arsalan Dehghani <14357506+arsalandehghani@users.noreply.github.com>
2021-12-21 13:08:27 +0330

    create function read_tsetmc_groups_dictionary

--- tsetmc/core.py
+++ tsetmc/core.py
@@ -7,3 +7,19 @@ import pandas as pd
 import requests
 import re
 
+def read_tsetmc_groups_dictionary():
+    url = 'http://www.tsetmc.com/Loader.aspx?ParTree=111C1213'
+    page = requests.get(url, allow_redirects=True)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    table_rows = soup.find_all('tr')
+    regexPattern = "<td>(\d+)\s+?</td>\s+?<td>(.*?)</td>"
+    dicGroups = {}
+    x = re.search(regexPattern, str(table_rows[1]), re.U)
+    for tr in table_rows:
+        x = re.search(regexPattern, str(tr), re.U)
+        if x:
+            code = x.group(1)
+            desc = x.group(2)
+            dicGroups[code] = desc
+    return dicGroups
+
