752c334e477b71112c741034bd30c3e5b5defa0f
Arsalan Dehghani <14357506+arsalandehghani@users.noreply.github.com>
2021-12-22 09:55:59 +0330

    Create New function read_tsetmc_groups_DataFrame and rename old read_tsetmc_groups_DataFrame

--- tsetmc/core.py
+++ tsetmc/core.py
@@ -2,6 +2,7 @@
 
 from bs4 import BeautifulSoup
 import datetime
+import numpy as np
 import os
 import pandas as pd
 import requests
@@ -27,7 +28,26 @@ def convert_dictionary_to_DataFrame(dic, keyColumnName='k', valueColumnName='v')
     new_data_frame = pd.DataFrame(list(dic.items()), columns=[keyColumnName, valueColumnName])
     return new_data_frame
 
-def read_tsetmc_groups_DataFrame():
+def read_tsetmc_groups_DataFrame1():
     dicGroups = read_tsetmc_groups_dictionary()
     dataFrameGroups = convert_dictionary_to_DataFrame(dicGroups, keyColumnName='groupKey', valueColumnName='groupName')
     return dataFrameGroups
+
+
+def read_tsetmc_groups_DataFrame():
+    url = 'http://www.tsetmc.com/Loader.aspx?ParTree=111C1213'
+    page = requests.get(url, allow_redirects=True)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    table_rows = soup.find_all('tr')
+    regexPattern = "<td>(\d+)\s+?</td>\s+?<td>(.*?)</td>"
+    arrGroups = np.array()
+    columns=['groupKey', 'groupName']
+    x = re.search(regexPattern, str(table_rows[1]), re.U)
+    for tr in table_rows:
+        x = re.search(regexPattern, str(tr), re.U)
+        if x:
+            code = x.group(1)
+            desc = x.group(2)
+            arrGroups.add([code, desc])
+    group_DataFrame = pd.DataFrame(arrGroups, columns)
+    return group_DataFrame
